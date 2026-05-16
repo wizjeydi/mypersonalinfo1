c83185f3f6f831088c1d04280ab75a7e708cf72e
wizjeydi <22-72104@g.batstate-u.edu.ph>
2026-05-16 18:38:53 +0000

    Remove profile section and update certificate list

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const certificates = [
-  { title: 'Frontend Developer - Coursera', issuer: 'Coursera', date: '2024', link: '#' },
-  { title: 'TypeScript Mastery', issuer: 'Udemy', date: '2023', link: '#' },
-  { title: 'UI/UX Design Professional', issuer: 'Adobe', date: '2024', link: '#' },
-  { title: 'React Development Certification', issuer: 'Meta', date: '2025', link: '#' },
-  { title: 'Responsive Web Design', issuer: 'Google', date: '2023', link: '#' },
+  { title: 'Applied Data Science with Python', issuer: '', date: 'November 30, 2024', link: '#' },
+  { title: 'Artificial Intelligence Fundamentals', issuer: '', date: 'November 30, 2024', link: '#' },
+  { title: 'English for IT 1', issuer: '', date: 'November 20, 2024', link: '#' },
+  { title: 'Big Data Foundations', issuer: '', date: 'November 29, 2024', link: '#' },
+  { title: 'Data Fundamentals', issuer: '', date: 'November 30, 2024', link: '#' },
+  { title: 'Data Analytics', issuer: '', date: 'May 14, 2025', link: '#' },
+  { title: 'CompTIA Network Security for Beginners & Professionals', issuer: '', date: 'September 25, 2025', link: '#' },
+  { title: 'HTML', issuer: '', date: 'December 01, 2022', link: '#' },
+  { title: 'CSS', issuer: '', date: 'December 02, 2022', link: '#' },
 ]
 
 const achievements = [
